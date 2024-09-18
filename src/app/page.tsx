fce6e61afb8ebca36a49b202d51faa4200278413
Zeno Leonardi <zenoelioleone@gmail.com>
2024-09-18 21:49:33 +0200

    Oppdaterte frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ async function fetchQR(url: string, size: number) {
   try {
     const resp = await fetch(
       "https://qrapi.fly.dev/?url=" + url + "&size=" + size
+      //"http://localhost:8080/?url=" + url + "&size=" + size
     );
     const text = await resp.text();
     return text;
@@ -38,21 +39,13 @@ export default function Home() {
       <div className="flex justify-center items-center h-screen">
         <Card className="w-[350px]">
           <CardHeader className="text-3xl font-semibold pb-4 text-center">
-            QR kode generator
+            Generate a QR-code
           </CardHeader>
           <CardContent>
             <div className="h-72 rounded-lg border bg-card text-card-foreground shadow p-4 ">
               {qrData && (
-                <div
-                  style={{ backgroundImage: qrData, height: 100, width: 100 }}
-                >
-                  <Image
-                    width={size || 64}
-                    height={size || 64}
-                    id="qrCode"
-                    src={qrData}
-                    alt="image"
-                  />
+                <div className="flex justify-center h-64 w-64">
+                  <img src={qrData} alt="image" />
                 </div>
               )}
             </div>
@@ -72,7 +65,7 @@ export default function Home() {
                   type="number"
                   id="size"
                   className="shadow py-2 px-3 mb-3 "
-                  placeholder="256"
+                  placeholder="256px"
                   onChange={(e) => setSize(e.target.valueAsNumber)}
                   min={64}
                   max={1028}
